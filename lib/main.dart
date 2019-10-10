631aec01f5054c528cfaf67111ecd4a7b6d10eb8
AshishTaduce <ashishteddy2@gmail.com>
2019-10-10 15:15:52 +0530

    done, static

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: TicTacToePage(),
+    theme: ThemeData(fontFamily: 'Proxima Nova Soft W03 Semibold.ttf'),
   ));
 }
 
@@ -12,12 +14,121 @@ class TicTacToePage extends StatefulWidget {
 }
 
 class _TicTacToePageState extends State<TicTacToePage> {
+
   @override
+
   Widget build(BuildContext context) {
+    var textHeight = MediaQuery.of(context).size.height * 0.10;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Play On!'),
+//      appBar: AppBar(
+//        centerTitle: true,
+//        title: Text('Play On!'),
+//        backgroundColor: Colors.blue.withAlpha(10),
+//      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black54,
+          image: DecorationImage(
+            image: AssetImage(
+              "assets/background.jpg",
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: textHeight,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Tic-Tac-Toe',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 42,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Player X to Move',
+                style: TextStyle(
+                  color: Colors.white.withAlpha(150),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Expanded(child: TicTacRow()),
+            Expanded(child: TicTacRow()),
+            Expanded(child: TicTacRow()),
+            FlatButton(
+              color: Colors.white.withAlpha(150),
+              onPressed: () {
+                /*...*/
+              },
+              child: Text(
+                "Reset",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: textHeight,
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class TicTacRow extends StatelessWidget {
+  const TicTacRow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: Colors.white.withAlpha(150),
+              borderRadius: BorderRadius.all(Radius.circular(12),),),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: Colors.white.withAlpha(150),
+              borderRadius: BorderRadius.all(Radius.circular(12),),),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: Colors.white.withAlpha(150),
+              borderRadius: BorderRadius.all(Radius.circular(12),),),
+          ),
+        ),
+      ],
     );
   }
 }
